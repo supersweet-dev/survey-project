f2c03e659a187beb3dfcfd51a81dd944090f1743
supersweet-dev <jim@supersweet.dev>
2024-01-23 18:50:04 -0600

    Init

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <SurveyForm/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import SurveyForm from './components/SurveyForm.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SurveyForm
   }
 }
 </script>
---
+++ src/components/SurveyForm.vue
@@ -0,0 +1,34 @@
+<template>
+  <form @submit.prevent="sendEmail">
+    <label for="birthday">Enter your birthday:</label>
+    <input type="date" id="birthday" v-model="birthday" required>
+    <button type="submit">Submit</button>
+  </form>
+</template>
+
+<script>
+import emailjs from 'emailjs-com';
+
+export default {
+  data() {
+    return {
+      birthday: ''
+    };
+  },
+  methods: {
+    sendEmail() {
+      const templateParams = {
+        birthday: this.birthday,
+        to_email: 'pentagr4mi@gmail.com',
+      };
+
+      emailjs.send('service_kjf34hk', 'template_bmvrb3f', templateParams, 'L_LV-itI8-cF7grnZ')
+        .then((response) => {
+          console.log('SUCCESS!', response.status, response.text);
+        }, (error) => {
+          console.log('FAILED...', error);
+        });
+    }
+  }
+};
+</script>
